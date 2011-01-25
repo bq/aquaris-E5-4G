d8c79c62034866e3a06a357004f3a2642e1572b5
Marc Zyngier <marc.zyngier@arm.com>
2011-01-25 15:54:20 +0000

    ARM: simplify percpu_timer_setup
    Now that all in-tree providers of local_timer_setup() are dynamically
    registering their timers with the per cpu timer code, remove the
    legacy timer selection code.
    
    If no local timer is provided, a broadcast timer will be used
    as a fallback.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/kernel/percpu_timer.c
+++ arch/arm/kernel/percpu_timer.c
@@ -15,7 +15,6 @@
 #include <linux/clockchips.h>
 
 #include <asm/localtimer.h>
-#include <asm/smp_twd.h>
 
 #ifdef CONFIG_GENERIC_CLOCKEVENTS_BROADCAST
 static void broadcast_timer_set_mode(enum clock_event_mode mode,
@@ -45,12 +44,7 @@ static struct local_timer_ops broadcast_timer_ops = {
 	.setup	= broadcast_timer_setup,
 };
 
-static struct local_timer_ops *timer_ops;
-
-int __attribute__ ((weak)) local_timer_setup(struct clock_event_device *evt)
-{
-	return -ENXIO;
-}
+static struct local_timer_ops *timer_ops = &broadcast_timer_ops;
 
 void percpu_timer_register(struct local_timer_ops *ops)
 {
@@ -107,18 +101,6 @@ void __cpuinit percpu_timer_setup(void)
 	int ret = 0;
 	unsigned int cpu = smp_processor_id();
 	struct clock_event_device *evt = &per_cpu(percpu_clockevent, cpu);
-
-	/*
-	 * All this can go away once we've migrated all users to
-	 * properly register the timer they use, and broadcast can
-	 * become the fallback.
-	 */
-	if (!timer_ops)
-		timer_ops = local_timer_get_twd_ops();
-	if (!timer_ops)
-		timer_ops = &broadcast_timer_ops;
-	if (!timer_ops->plat_setup)
-		timer_ops->plat_setup = local_timer_setup;
 
 	evt->cpumask = cpumask_of(cpu);
 
