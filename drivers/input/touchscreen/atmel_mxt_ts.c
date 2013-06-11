d78163ca0fa01c2c0f7e22cd8ca1c150223f41f6
Nick Dyer <nick.dyer@itdev.co.uk>
2013-06-11 15:32:02 +0100

    Input: atmel_mxt_ts - Fail probe if info block CRC zero
    It is possible with buggy comms that we might read all zeros, in which case
    the driver should abort probe rather than trying to proceed.
    
    Signed-off-by: Nick Dyer <nick.dyer@itdev.co.uk>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -1894,7 +1894,7 @@ static int mxt_read_info_block(struct mxt_data *data)
 
 	/* CRC mismatch can be caused by data corruption due to I2C comms
 	 * issue or else device is not using Object Based Protocol */
-	if (data->info_crc != calculated_crc) {
+	if ((data->info_crc == 0) || (data->info_crc != calculated_crc)) {
 		dev_err(&client->dev,
 			"Info Block CRC error calculated=0x%06X read=0x%06X\n",
 			data->info_crc, calculated_crc);
